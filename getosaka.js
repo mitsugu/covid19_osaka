b03caca46540033dc11512cbb154c03d940f05c9
mitsugu oyama <mitsugu.oyama@gmail.com>
2020-08-25 08:05:53 +0900

    fix incorrect path

--- getosaka.js
+++ getosaka.js
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 
 const fs = require("fs");
-fs.readFile("/home/mitsugu/Documents/Medical information/SARS-CoV-2/covid19/data/data.json",{ encoding: "utf8" }, (err, file) => {
+fs.readFile("./covid19osaka/data/data.json",{ encoding: "utf8" }, (err, file) => {
   if (err) {
     console.error(err);
   } else {
