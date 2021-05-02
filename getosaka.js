6bedb9b619965969b749ac989f1c6d3cac5a79e5
mitsugu oyama <mitsugu.oyama@gmail.com>
2021-05-02 18:17:28 +0900

    軽症者のカウント方法がまずいので変更&症状不明を追加

--- getosaka.js
+++ getosaka.js
@@ -7,19 +7,20 @@ fs.readFile("./data.json",{ encoding: "utf8" }, (err, file) => {
   } else {
     var obj = JSON.parse( file );
     var d       = obj.lastUpdate;
-    var taiin   = obj.main_summary.children[0].children[1].value;             // 退院
-    var shibou  = obj.main_summary.children[0].children[2].value;             // 死亡
-    var jyuu    = obj.main_summary.children[0].children[0].children[1].value; // 入院・重症
-
-    var kei     = obj.main_summary.children[0].children[0].value              // 入院／調整中
-                - obj.main_summary.children[0].children[0].children[1].value  // 入院・重症
-                + obj.main_summary.children[0].children[3].value              // 自宅療養
-                + obj.main_summary.children[0].children[4].value              // 宿泊療養
-                + obj.main_summary.children[0].children[5].value              // 療養等調整中
-                + obj.main_summary.children[0].children[6].value              // 入院調整中
-                + obj.main_summary.children[0].children[7].value;             // 符外健康観察
+                                                                    // 入院・軽症
+    var kei     = obj.main_summary.children[0].children[0].children[0].value;
+                                                                    // 入院・重症
+    var jyuu    = obj.main_summary.children[0].children[0].children[1].value;
+    var fumei   = obj.main_summary.children[0].children[3].value    // 自宅療養
+                + obj.main_summary.children[0].children[4].value    // 宿泊療養
+                + obj.main_summary.children[0].children[5].value    // 療養等調整中
+                + obj.main_summary.children[0].children[6].value    // 入院調整中
+                + obj.main_summary.children[0].children[7].value;   // 府外健康観察
+    var shibou  = obj.main_summary.children[0].children[2].value;   // 死亡
+    var taiin   = obj.main_summary.children[0].children[1].value;   // 退院
     console.log( "\"" + d + "\"" + ", "
                   + kei + ", "
+                  + fumei + ", "
                   + jyuu + ",, "
                   + shibou + ", "
                   + taiin );
